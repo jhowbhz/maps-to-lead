d7e73c885e027dc443216d4f8a1db01a4afec495
Jonathan Henrique <jhowjhoe@icloud.com>
2022-11-16 04:34:42 -0300

    remove ddi

--- Class/Parser.js
+++ Class/Parser.js
@@ -3,18 +3,16 @@ module.exports = class Parser {
 
     static checkNumber(dados) {
 
-        let pais = '55';
-
         if(dados?.infos[1]?.replace(/\D/g, '').length > 9 && dados?.infos[1]?.replace(/\D/g, '').length < 15){
-            return `${pais}${dados.infos[1].replace(/\D/g, '')}`;
+            return `${dados.infos[1].replace(/\D/g, '')}`;
         } else if(dados?.infos[2]?.replace(/\D/g, '').length > 9 && dados?.infos[2]?.replace(/\D/g, '').length < 15){
-            return `${pais}${dados.infos[2].replace(/\D/g, '')}`;
+            return `${dados.infos[2].replace(/\D/g, '')}`;
         } else if(dados?.infos[3]?.replace(/\D/g, '').length > 9 && dados?.infos[3]?.replace(/\D/g, '').length < 15){
-            return `${pais}${dados.infos[3].replace(/\D/g, '')}`;
+            return `${dados.infos[3].replace(/\D/g, '')}`;
         } else if(dados?.infos[4]?.replace(/\D/g, '').length > 9 && dados?.infos[4]?.replace(/\D/g, '').length < 15){
-            return `${pais}${dados.infos[4].replace(/\D/g, '')}`;
+            return `${dados.infos[4].replace(/\D/g, '')}`;
         } else if (dados?.infos[5]?.replace(/\D/g, '').length > 9 && dados?.infos[5]?.replace(/\D/g, '').length < 15){
-            return `${pais}${dados.infos[5].replace(/\D/g, '')}`;
+            return `${dados.infos[5].replace(/\D/g, '')}`;
         }
         return '';
     }
